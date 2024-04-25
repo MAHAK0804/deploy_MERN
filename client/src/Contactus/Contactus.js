ebe1ccdbd3565c920d5d1521e6213e9e3c25b306
MAHAK <mahak191713@gmail.com>
2024-04-26 01:24:24 +0530

    first commit

--- client/src/Contactus/Contactus.js
+++ client/src/Contactus/Contactus.js
@@ -38,7 +38,7 @@ export default function Contactus() {
 
     }
     axios.post('https://deploy-mern-api.vercel.app/Contact',details)
-    navigate("/Contact");
+    navigate("/");
 
   }
  
@@ -81,7 +81,7 @@ export default function Contactus() {
         <br />
         <div class='container'>
 
-      <form class="row g-3 needs-validation"  method='post' validate action='/Contact'>
+      <form class="row g-3 needs-validation"  method='post' validate action='/'>
   <div class="col-md-6">
     <label for="validationCustom01" class="form-label">Name</label>
     <input type="text" class="form-control" id="validationCustom01" placeholder="Enter Your Name" name="name" value={input.name}  onChange={handleChange} required />
