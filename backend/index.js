89d47f6d09318b72fc13ab08cdaacebfff627147
MAHAK <mahak191713@gmail.com>
2024-04-26 01:03:56 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin : ("https://deploy-mern-delta.vercel.app/"),
+    origin : ("https://deploy-mern-delta.vercel.app"),
     methods:['POST','GET'],
     credentials:true
 
